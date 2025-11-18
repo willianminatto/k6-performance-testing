778ec41bea6b490daee37513efb408b67bd374a5
willian <135767840+willianminatto@users.noreply.github.com>
2025-11-18 13:28:13 -0300

    Increase test load and update performance report
    Comment out all code in Tests.spec.js to disable tests.

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,51 +1,51 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-import http from 'k6/http';
-import { check } from 'k6';
-import { Trend, Rate } from 'k6/metrics';
+// import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+// import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+// import http from 'k6/http';
+// import { check } from 'k6';
+// import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts_duration', true);
-export const rateStatusCodeOK = new Rate('rate_status_code_ok');
+// export const getContactsDuration = new Trend('get_contacts_duration', true);
+// export const rateStatusCodeOK = new Rate('rate_status_code_ok');
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.12'],
-    http_req_duration: ['p(95)<5700'],
-    rate_status_code_ok: ['rate>0.95']
-  },
-  stages: [
-    { duration: '30s', target: 10 },
-    { duration: '1m', target: 100 },
-    { duration: '1m30s', target: 200 },
-    { duration: '2m', target: 300 }
-  ]
-};
+// export const options = {
+//   thresholds: {
+//     http_req_failed: ['rate<0.12'],
+//     http_req_duration: ['p(95)<5700'],
+//     rate_status_code_ok: ['rate>0.95']
+//   },
+//   stages: [
+//     { duration: '30s', target: 10 },
+//     { duration: '1m', target: 100 },
+//     { duration: '1m30s', target: 200 },
+//     { duration: '2m', target: 300 }
+//   ]
+// };
 
-export function handleSummary(data) {
-  return {
-    './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
-  };
-}
+// export function handleSummary(data) {
+//   return {
+//     './src/output/index.html': htmlReport(data),
+//     stdout: textSummary(data, { indent: ' ', enableColors: true })
+//   };
+// }
 
-export default function () {
-  const baseUrl = 'http://servicodados.ibge.gov.br/api/v3/noticias/';
+// export default function () {
+//   const baseUrl = 'http://servicodados.ibge.gov.br/api/v3/noticias/';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+//   const params = {
+//     headers: {
+//       'Content-Type': 'application/json'
+//     }
+//   };
 
-  const OK = 200;
+//   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+//   const res = http.get(`${baseUrl}`, params);
 
-  getContactsDuration.add(res.timings.duration);
+//   getContactsDuration.add(res.timings.duration);
 
-  rateStatusCodeOK.add(res.status === OK);
+//   rateStatusCodeOK.add(res.status === OK);
 
-  check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
-  });
-}
+//   check(res, {
+//     'GET Contacts - Status 200': () => res.status === OK
+//   });
+// }
